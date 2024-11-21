dda2756ebfe9e14ab7302d2367abd694bcad3a41
Johan Medrano <johan.medrano@ucl.ac.uk>
2024-11-21 11:05:06 +0000

    Update with structarray

--- templates/wrapper.py
+++ templates/wrapper.py
@@ -1,15 +1,15 @@
 try:
-	from <pkgname>._<pkgname> import initialize
+    from <pkgname>._<pkgname> import initialize
 except ImportError as e:
-	import os
-	installer_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 
-        '_<pkgname>', 
+    import os
+    installer_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '_spm',
         'resources',
         'RuntimeInstaller.install')
-
 	print("Failed to import, install Matlab Runtime and setup library path. ")
 	print(f"Matlab Runtime installer can be found in: {installer_path}")
+
 	raise e
 import warnings
 import numpy as np
@@ -172,7 +172,7 @@ class Runtime:
 
     @staticmethod
     def _cast_argin(arg):
-        if isinstance(arg, MatlabClassWrapper):
+        if isinstance(arg, (MatlabClassWrapper, StructArray)):
             arg = arg._as_matlab_object()
         if isinstance(arg, dict):
             _, arg = Runtime._process_argin(**arg)
@@ -210,6 +210,9 @@ class Runtime:
                     else:
                         warnings.warn(f'Unknown Matlab class type: {res["type__"]}')
                         out = MatlabClassWrapper(_objdict=res)
+                elif res['type__'] == 'structarray':
+                    print()
+                    out = StructArray._from_matlab_object(res)
                 else:
                     out = res
             else:
@@ -244,3 +247,94 @@ class Cell(list):
             self[i] = xi
         else:
             super().__setitem__(index, value)
+
+
+
+class StructArray:
+    def __init__(self, *structs):
+        if len(structs) == 1:
+            if isinstance(structs[0], Struct):
+                structs = [structs[0]]
+            else:
+                if all(map(isinstance, structs[0],itertools.repeat(int))):
+                    size = structs[0]
+                    structs = np.fromiter(
+                        map(
+                            lambda i: dict(), range(np.prod(size))),
+                        dtype=object).reshape(size)
+                elif all(map(isinstance, structs[0], itertools.repeat(dict))):
+                    structs = structs[0]
+                elif isinstance(structs[0], np.ndarray) \
+                        and all(map(isinstance, structs[0].flat, itertools.repeat(dict))):
+                    structs = structs[0]
+                else:
+                    raise TypeError(f'arguments not understood: {structs}')
+
+        if isinstance(structs, np.ndarray):
+            structs = np.fromiter(
+                map(dict, structs.flat),
+                dtype=object).reshape(structs.shape)
+        else:
+            structs = np.fromiter(
+                map(dict, structs),
+                dtype=object)
+
+        if len(structs.shape) == 1:
+            structs = structs[None, :]
+
+        self._structs = structs
+        self._objdict = dict(
+            type__='structarray',
+            size__=np.array(structs.shape),
+            data__=[]
+        )
+
+    def __getitem__(self, index):
+        try:
+            len(index)
+        except TypeError:
+            index = (0, index)
+
+        item = self._structs[index]
+        if isinstance(item, dict):
+            item = Struct(item)
+        return item
+
+    def keys(self):
+        return set(
+            itertools.chain.from_iterable(
+                map(dict.keys, self._structs.flat)))
+
+    def _as_matlab_object(self):
+        _ = [*map(
+            lambda arg: arg[0].__setitem__(arg[1], np.array([])),
+            filter(
+                lambda arg: arg[1] not in arg[0].keys(),
+                itertools.product(self._structs.flat, self.keys())
+            )
+        )]
+        objdict = self._objdict
+        objdict['data__'] = self._structs.tolist()
+        return objdict
+
+    def __repr__(self):
+        return self._structs \
+            .__repr__() \
+            .replace('array([], dtype=float64)', 'empty') \
+            .replace('matlab.double([])', 'empty') \
+            .replace('array(', 'StructArray(\n  data=') \
+            .replace(', dtype=object', f',\n  keys={self.keys()}')
+
+    @staticmethod
+    def _from_matlab_object(objdict):
+        if objdict['type__'] != 'structarray':
+            raise TypeError('objdict is not a structarray')
+        size = tuple(map(int, objdict['size__'][0]))
+        data = np.fromiter(objdict['data__'], dtype=object)
+        data = data.reshape(size)
+        try:
+            obj = StructArray(data)
+        except Exception as e:
+            raise RuntimeError(f'Failed to construct StructArray data:\n  data={data}\n  objdict={objdict}')
+
+        return obj
